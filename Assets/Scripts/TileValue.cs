0446f8a1f6cd49310f280c4294e15ee9acd4d37a
Xwilarg <xwilarg@yahoo.fr>
2019-05-31 11:33:37 +0200

    Add tiles

---
+++ Assets/Scripts/TileValue.cs
@@ -0,0 +1,13 @@
+﻿public struct TileValue
+{
+    public enum TileColor
+    {
+        Red,
+        Black,
+        Yellow,
+        Blue
+    }
+
+    public int value; // 0: Joker
+    public TileColor color;
+}
